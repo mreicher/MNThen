92eeeefbdbd50d7b479cdee73e74f8c35983ffb0
mreicher <42914553+mreicher@users.noreply.github.com>
2023-05-25 14:48:00 -0500

    Create locations.js
    Adding blue markers on the map -- each tour map will read from the same JS file. This will (feasibly) allow users to go beyond the range of a typical tour to hunt for other blue markers.

---
+++ locations.js
@@ -0,0 +1,11 @@
+var locations = [
+  {lat: 45.1442221, lng: -93.0042531, name: "Marker 1"},
+  {lat: 45.1439884, lng: -93.0038333, name: "Marker 2"},
+  {lat: 45.1439526, lng: -93.0033916, name: "Marker 3"},
+  {lat: 45.1441365, lng: -93.0031786, name: "Marker 4"},
+  //inside the Capitol
+  {lat: 44.955674, lng: -93.1069443, name: "Rathskeller"},
+    {lat: 44.9565977, lng: -93.1028148, name: "MN Senate Building"}
+  //Capitol Tour
+  
+];
