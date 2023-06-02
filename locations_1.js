81761ba0f7d431e4ab3a778825db7c93a44c47d3
mreicher <42914553+mreicher@users.noreply.github.com>
2023-06-02 06:10:19 -0500

    Update locations_1.js
    Added new tour location for Swede Hollow

--- locations_1.js
+++ locations_1.js
@@ -10,10 +10,17 @@ var locations = [
     lat: 44.96244516,
     lng: -93.07088783,
     name: "Minnesota 13 Whiskey",
-    popupContent: "Location 2 Popup Content",
+    popupContent: "11 Wells",
     htmlFile: "/tour/hamm/hamm_mn13.html"
   },
   {
+    lat: 44.96147364579489,
+    lng: -93.07331340101548,
+    name: "Swede Hollow",
+    popupContent: "Above Swede Hollow",
+    htmlFile: "/tour/hamm/hamm_hollow.html"
+  },
+    {
     lat: 44.962043,
     lng: -93.072014,
     name: "Location 3",
