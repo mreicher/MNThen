7f51d85e76c755cde7840149b1b5a7de5f1bb978
mreicher <42914553+mreicher@users.noreply.github.com>
2023-06-02 16:46:38 -0500

    Create locations_1.js
    Testing the map

---
+++ tour/hamm/locations_1.js
@@ -0,0 +1,44 @@
+var locations = [
+  {
+    lat: 45.144334537396375,
+    lng: -93.00439140908348,
+    name: "The Logic Test",
+    popupContent: "Logic Tester",
+    htmlFile: "/example/logic_test.html"
+  },
+  {
+    lat: 44.96244516,
+    lng: -93.07088783,
+    name: "Minnesota 13 Whiskey",
+    popupContent: "11 Wells",
+    htmlFile: "/tour/hamm/hamm_mn13.html"
+  },
+  {
+    lat: 44.96147364579489,
+    lng: -93.07331340101548,
+    name: "Swede Hollow",
+    popupContent: "Above Swede Hollow",
+    htmlFile: "/tour/hamm/hamm_hollow.html"
+  },
+   {
+    lat: 44.9620906324505,
+    lng: -93.0710358080048,
+    name: "Hamm Jr. Kidnapping",
+    popupContent: "Hamm Jr. Kidnapping",
+    htmlFile: "/tour/hamm/hamm_kidnap.html"
+  },
+      {
+    lat: 44.9626001898243,
+    lng:  -93.07036032373,
+    name: "Location Template",
+    popupContent: "Closing of Stroh Brewery",
+    htmlFile: "/tour/hamm/hamm_closing.html"
+  },
+    {
+    lat: #,
+    lng: #,
+    name: "Location Template",
+    popupContent: "Location 3 Popup Content",
+    htmlFile: "location3.html"
+  }
+];
