a597359ce12a67f36c4b6798d7a1319911ca74f1
mreicher <42914553+mreicher@users.noreply.github.com>
2023-07-17 11:53:11 -0500

    Create locations.js
    Changing the file location.

---
+++ locations/data/locations.js
@@ -0,0 +1,126 @@
+// locations.js
+
+var locations = [
+  {
+    name: "Minnesota 13 Whiskey",
+    lat: 44.96244516,
+    lon: -93.07088783,
+    image: "https://www.mnthen.com/tour/hamm/gallery/mn13/hamm_ms13_1.jpg",
+    title: "Minnesota 13",
+    creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3039852975572-moonshine-legacy-the-story-of-minnesota-13-and-stearns-county-s-bootlegging-era" // Replace with the actual URL of the article
+  },
+   {
+    name: "Swede Hollow",
+    lat: 44.96147364579489,
+    lon: -93.07331340101548,
+    image: "https://www.mnthen.com/tour/hamm/gallery/hollow/hamm_hollow1.jpg",
+    title: "Swede Hollow",
+    creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2905067413178-swede-hollow"
+  },
+  //fix these
+   {
+    name: "Hamm Jr. Kidnapping (1933)",
+    lat: 44.9620906324505,
+    lon: -93.0710358080048,
+     image: "https://www.mnthen.com/tour/hamm/gallery/kidnap/hamm_kidnap1.jpg",
+    title: "Hamm Jr. Kidnapping",
+    creator: "Matt Reicher",
+    link: "https://vocal.media/criminal/kidnapping-of-hamm-s-brewery-president-william-hamm-jr-june-15-19-1933"
+  },
+      {
+    name: "Closing of Stroh Brewery (1997)",
+    lat: 44.9626001898243,
+     lon: -93.07036032373,
+      image: "https://www.mnthen.com/tour/hamm/gallery/closing/hamm_closing1.jpg",
+    title: "Closing of the Stroh Brewery",
+    creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2922345876111-closing-of-east-st-paul-s-stroh-hamm-s-brewery-november-21-1997"
+  },
+    {
+    name: "The Iconic Hamm's Bear (1953 - 99)",
+    lat: 44.96231983329402,
+     lon: -93.07176137488261,
+      image: "https://www.mnthen.com/tour/hamm/gallery/bear/hamm_bear1.jpg",
+    title: "Hamm's Bear",
+    creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2918808919766-the-story-of-the-iconic-hamm-s-bear-1953-1999"
+  },          
+  {
+    name: "'New' Hamm's Brewery Public Tour (1894)",
+    lat: 44.96292536959989,
+     lon:  -93.07205849805618,
+      image: "https://www.mnthen.com/tour/hamm/gallery/public/hamm_public1.jpg",
+     title: "Hamm's Brewery Tour",
+     creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2928741186662-public-tour-of-the-new-hamm-s-brewery-september-27-1894"
+  },
+    {
+    name: "Theodore Hamm Mansion (1886 - 1954)",
+    lat: 44.961337319371076,
+    lon:  -93.07148153924896,
+     image: "https://www.mnthen.com/tour/hamm/gallery/mansion/hamm_mansion1.jpg",
+     title: "Theodore Hamm Mansion",
+     creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2899577700997-theodore-hamm-mansion-c-1886-1954"
+  },
+      {
+    name: "Stahlmann Brewery (1855 - 97)",
+    lat: 44.928296016597855,
+     lon:   -93.12476627530773,
+      image: "https://img.particlenews.com/image.php?type=thumbnail_580x000&url=1VFflC_0mkCicPj00",
+     title: "Stahlmann Brewery",
+     creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3046980377052-the-legacy-lives-on-uncovering-the-little-known-story-of-christopher-stahlmann-s-brewing-empire"
+  },
+  {
+    name: "Scandal at John's Bar (1952 - 56)",
+    lat: 45.013359183182786,
+     lon:  -93.27190590645603,
+      image: "https://img.particlenews.com/image.php?type=thumbnail_580x000&url=2hntzQ_0mivQGBS00",
+     title: "Scandal at John's Bar (1952 - 56)",
+     creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3045699798148-unveiling-the-secrets-of-john-s-bar-and-funhouse-a-scandalous-tale-of-corruption-in-1950s-minneapolis"
+  },
+    {
+    name: "Bruce Vento (1940 - 2000)",
+    lat: 44.95270480002584,
+     lon:   -93.07440808976824,
+      image: "https://img.particlenews.com/image.php?type=thumbnail_580x000&url=4Mv2y5_0mmgW4bE00",
+     title: "Bruce Vento (1940 - 2000)",
+     creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3049434704183-prominent-politician-and-advocate-bruce-f-vento-s-enduring-legacy-of-social-justice-and-environmental-conservation"
+  },
+  {
+    name: "Plantation Night Club (1930 - 1933)",
+    lat: 45.073692373690626,
+    lon:  -93.01483408611226,
+    image: "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant1.jpg",
+    title: "Plantation Night Club",
+    creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club"
+  },
+    {
+    name: "The Geist Gazebo",
+    lat: 45.08631531351397,
+    lon:  -92.99990683456114,
+    image: "https://www.mnthen.com/tour/wbl/gallery/geist/geist_2.jpg",
+    title: "The Geist Gazebo",
+    creator: "Matt Reicher",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3077047712038-matoska-park-s-living-history-the-geist-gazebo-and-its-storied-past"
+  },
+      {
+    name: "Hubert H. Humphrey (1911 - 1978)",
+    lat: 44.95392488222734,
+    lon:  -93.1032724056789,
+    image: "https://www.mnthen.com/tour/capitol/gallery/humphrey/humphrey_3.jpg",
+    title: "Hubert H. Humphrey (1911 - 1978)",
+    creator: "Matt Reicher",
+    link: "https://mnthen.com/blog/people/humphrey.html"
+  }
+  // Add more locations here
+];
+
+// Export the locations array
+export default locations;
