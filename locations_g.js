c2adaa6f9b886b85e6dc7ec08453fd503653c26d
mreicher <42914553+mreicher@users.noreply.github.com>
2023-08-28 20:23:38 -0500

    Create locations_g.js
    Created a separate Gangster History Tour location array.

---
+++ locations_g.js
@@ -0,0 +1,45 @@
+// locations.js
+const locations = [
+  {
+    name: "Minnesota 13 Whiskey",
+    lat: 44.96244516,
+    lng: -93.07088783,
+    image: "https://www.mnthen.com/tour/hamm/gallery/mn13/hamm_ms13_1.jpg",
+    title: "Minnesota 13",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_mn13.html",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3039852975572-moonshine-legacy-the-story-of-minnesota-13-and-stearns-county-s-bootlegging-era" // Replace with the actual URL of the article
+  },
+   {
+    name: "Hamm Jr. Kidnapping (1933)",
+    lat: 44.9620906324505,
+    lng: -93.0710358080048,
+    image: "https://www.mnthen.com/tour/hamm/gallery/kidnap/hamm_kidnap1.jpg",
+    title: "Hamm Jr. Kidnapping",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_kidnap.html",
+    link: "https://vocal.media/criminal/kidnapping-of-hamm-s-brewery-president-william-hamm-jr-june-15-19-1933"
+  },
+  {
+    name: "Plantation Night Club (1930 - 1933)",
+    lat: 45.073692373690626,
+    lng:  -93.01483408611226,
+    image: "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant1.jpg",
+    title: "Plantation Night Club",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/wbl/wbl_plant.html",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club"
+  },
+  {
+    name: "The Last Moments of Homer Van Meter",
+    lat: 44.955601674534925,
+    lng: -93.10899976598101,
+    image: "https://www.mnthen.com/images/van_meter_header.jpg",
+    title: "The Death of Gangster Homer Van Meter (Aug 23, 1934)",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/gangster/gangster_van_meter.html",
+    link: "https://mnthen.com/blog/people/van_meter.html"
+  },
+
+  // Add more locations here
+];
