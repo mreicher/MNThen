e5b146393bb8dbe46b2a2b3d5c6fd9df62654f06
mreicher <42914553+mreicher@users.noreply.github.com>
2023-09-01 19:13:15 -0500

    Update locations.js

--- locations/data/locations.js
+++ locations/data/locations.js
@@ -19,9 +19,8 @@ var locations = [
     creator: "Matt Reicher",
     link: "https://original.newsbreak.com/@ron-dansley-1587459/2905067413178-swede-hollow"
   },
-  //fix these
    {
-    name: "Hamm Jr. Kidnapping (1933)",
+    name: "Hamm Jr. Kidnapping (June 15-19, 1933)",
     lat: 44.9620906324505,
     lon: -93.0710358080048,
      image: "https://www.mnthen.com/tour/hamm/gallery/kidnap/hamm_kidnap1.jpg",
