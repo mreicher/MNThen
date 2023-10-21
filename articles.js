4fbda574e84dd9bbd49982c2b84cb68a23d2a406
mreicher <42914553+mreicher@users.noreply.github.com>
2023-10-21 13:12:52 -0500

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,193 @@
+function getTourCards() {
+  return [
+    {
+      title: "Kidnapping of Edward Bremer (Jan 17 - Feb 7, 1934)",
+      summary: "Ed Bremer was kidnapped and held for ransom by the Barker-Karpis gang in 1934. His eventual release helped bring about the end of the gangster era.",
+      creator: "Matt Reicher",
+      image: "https://mnthen.com/images/gangster_bremer_kidnap1.jpg",
+      link: "https://mnthen.com/blog/event/bremer_kidnap.html",
+      keywords: ["bremer", "kidnap", "kidnapping", "gangster"],
+            categories: ["History", "Architecture"],
+    },
+    {
+      title: "Hanging of the Dakota 38 (Dec. 26, 1862)",
+      summary: "On December 26, 1862, the largest mass hanging in American history marked the end of the US-Dakota War, with 38 condemned individuals executed.",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/markers/mankato/images/mankato_38.jpg",
+      link: "https://mnthen.com/blog/event/dakota_war.html",
+      keywords: ["dakota", "war", "mankato", "sibley"],
+            categories: ["History", "Architecture"],
+    },
+        {
+      title: "Shootout at the Lincoln Court Apartments (March 31, 1934)",
+      summary: "Dillinger and Van Meter engaged in epic gun battle with police at Lincoln Court Apartments in St. Paul, galvanizing public sentiment against gangster violence plaguing the city.",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/images/gangster/gangster_lincoln_ct.jpg",
+      link: "https://mnthen.com/blog/event/shootout_lincoln_ct.html",
+      keywords: ["gangster", "lincoln", "Lincoln", "Dillinger", "dillinger"],
+            categories: ["History", "Architecture"],
+    },
+    {
+      title: "Shooting of Christmas Wreath Salesman Oscar Erickson (Dec. 16, 1932)",
+      summary: "Innocent wreath salesman Oscar Erickson was killed by the Barker-Karpis gang during a robbery getaway, fueling calls to end the era of brazen criminals terrorizing citizens.",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/images/erickson.jpg",
+      link: "https://mnthen.com/blog/people/erickson.html",
+      keywords: ["gangster", "barker", "karpis", "shooting"],
+            categories: ["History", "Architecture"],
+    },
+        {
+      title: "Larsen, Michael 'Eyedea' (Nov 9, 1981 - Oct 16, 2010)",
+      summary: "Gifted battle rapper and versatile hip-hop artist Eyedea left a profound musical impact despite a tragic accidental overdose death at 28.",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/images/wsp_eyedea.jpg",
+      link: "https://mnthen.com/blog/people/eyedea_larsen.html",
+      keywords: ["eyedea", "smile", "rap", "music", "Eyedea"],
+            categories: ["History", "Architecture"],
+    },
+    {
+      title: "Humphrey, Hubert H. (1911 - 1978)",
+      summary: "Progressive champion Hubert Humphrey left an enduring legacy fighting tirelessly for civil rights and social justice as mayor, senator, vice president.",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/images/humphrey_header.jpg",
+      link: "https://mnthen.com/blog/people/humphrey.html",
+      keywords: ["humphrey", "Humphrey", "politics", "minneapolis", "Minneapolis"],
+            categories: ["History", "Architecture"],
+    },
+        {
+      title: "Olson, Floyd B. (1891 - 1936)",
+      summary: "Floyd B. Olson, Minnesota's 22nd Governor, left a profound legacy with his unwavering commitment to social justice, labor rights, and economic reforms during the Great Depression. ",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/tour/capitol/gallery/olson/olson_1.jpg",
+      link: "https://mnthen.com/blog/people/olson.html",
+      keywords: ["politics", "govenor", "olson", "Olson"],
+            categories: ["History", "Architecture"],
+    },
+        {
+      title: "Van Meter, Homer V. (1905 - 1934)",
+      summary: "Van Meter, an infamous Depression-era gangster, met his violent end in a disputed encounter with St. Paul police on August 23, 1934, near University Ave. and Marion St.",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/images/van_meter_header.jpg",
+      link: "https://mnthen.com/blog/people/van_meter.html",
+      keywords: ["gangster", "dillinger", "Dillinger", "shooting", "van meter", "Van Meter"],
+            categories: ["History", "Architecture"],
+    },
+    {
+      title: "Grain Belt Beer Sign",
+      summary: "In 1941, the iconic Grain Belt Beer sign, a giant bottle cap, was erected in Minneapolis. It went dark in 1996 but was relit in 2017 after restoration efforts.",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/locations/gallery/mpls/premium/mpls_grain_belt_1.jpg",
+      link: "https://mnthen.com/blog/place/mpls_grain_belt.html",
+      keywords: ["premium", "grain belt", "sign", "minneapolis", "Minneapolis", "beer"],
+            categories: ["History", "Architecture"],
+    },
+            {
+      title: "Gramma B's",
+      summary: "Floyd B. Olson, Minnesota's 22nd Governor, left a profound legacy with his unwavering commitment to social justice, labor rights, and economic reforms during the Great Depression. ",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/locations/gallery/mpls/gramma_b/mpls_gramma_b_1.jpg",
+      link: "https://mnthen.com/blog/place/mpls_gramma_b.html",
+      keywords: ["northeast", "Northeast", "Minneapolis", "saloon", "bar"],
+            categories: ["History", "Architecture"],
+    },
+        {
+      title: "Summit Brewing Company (1986 - )",
+      summary: "Founded by Mark Stutrud, Summit grew from a small brewery focusing on underrepresented beer styles to one of Minnesota's largest breweries.",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/markers/stp/summit/images/stp_summit_1.jpg",
+      link: "https://mnthen.com/blog/place/stp_summit_brewing.html",
+      keywords: ["saint paul", "st paul", "Summit", "summit", "beer", "stutrud", "Stutrud"],
+            categories: ["History", "Architecture"],
+    },
+    {
+      title: "Native American Burial Mounds (White Bear Lake)",
+      summary: "In 1941, the iconic Grain Belt Beer sign, a giant bottle cap, was erected in Minneapolis. It went dark in 1996 but was relit in 2017 after restoration efforts.",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/tour/wbl/gallery/mound/wbl_mound1.jpg",
+      link: "https://mnthen.com/blog/place/wbl_mounds.html",
+      keywords: ["burial", "mounds", "bear", "Bear", "lake"],
+            categories: ["History", "Architecture"],
+    },
+        {
+      title: "Wildwood Amusement Park",
+      summary: "Wildwood Amusement Park on White Bear Lake, the 'Coney Island of Saint Paul,' entertained generations until financial woes forced its closure in the 1930s",
+            creator: "Matt Reicher",
+      image: "https://mnthen.com/locations/gallery/mahtomedi/wildwood/wildwood_2.jpg",
+      link: "https://mnthen.com/blog/place/wildwood.html",
+      keywords: ["amusement", "park", "mahtomedi", "wildwood", "streetcar"],
+            categories: ["History", "Architecture"],
+    },
+  ];
+}
+
+function openArticleInPopup(link) {
+  const popup = document.getElementById('articlePopup'); // Get the popup div
+  const articleIframe = document.getElementById('articleIframe'); // Get the iframe inside the popup
+
+  // Set the iframe source to the article link
+  articleIframe.src = link;
+
+  // Display the popup
+  popup.style.display = 'block';
+}
+
+function closePopup() {
+  const popup = document.getElementById('articlePopup'); // Get the popup div
+
+  // Hide the popup
+  popup.style.display = 'none';
+}
+
+function generateArticleCards() {
+  const articles = getTourCards();
+  const articleContainer = document.getElementById('articleRow');
+
+  articles.forEach((article, index) => {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const image = document.createElement('img');
+    image.src = article.image;
+    image.className = 'card-img-top';
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    const title = document.createElement('h5');
+    title.className = 'card-title recent-article-title';
+    title.textContent = article.title;
+
+    const summary = document.createElement('p');
+    summary.className = 'card-text recent-article-summary';
+    summary.textContent = article.summary;
+
+
+    const creator = document.createElement('p');
+    creator.className = 'card-text';
+    creator.textContent = 'By ' + article.creator;
+
+    const link = document.createElement('a');
+    link.href = 'javascript:void(0)'; // This prevents the default link behavior
+    link.className = 'btn btn-primary';
+    link.textContent = 'Read More';
+    link.addEventListener('click', () => openArticleInPopup(article.link)); // Open in popup
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(summary);
+    cardBody.appendChild(creator);
+    cardBody.appendChild(link);
+
+    card.appendChild(image);
+    card.appendChild(cardBody);
+
+    articleContainer.appendChild(card);
+  });
+}
+
+// Call the generateArticleCards function when the page loads
+generateArticleCards();
+
+// Export the function to make it accessible from other scripts
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = { getTourCards };
+}
