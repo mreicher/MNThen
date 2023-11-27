fab76cfb9493975a20545af985e35c0b8d8288a2
mreicher <42914553+mreicher@users.noreply.github.com>
2023-11-26 21:09:00 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,45 +1,40 @@
 // questions.js
 
 const questions = [
+  {
+    question: "First keeper of John O'Connor's layover agreement system?",
+    answers: ["John Dillinger", "William Griffin", "'Dapper' Dan Hogan", "Alvin Karpis"],
+    correctAnswer: "William Griffin",
+    tidbit: "After arriving in town and meeting with the police, criminals stopped to 'check in' with Griffin at the Hotel Savoy in downtown St. Paul. Griffin collected bribes and brought the money to O'Connor."
+  },
+  {
+    question: "What earned Dan Hogan the nickname 'Dapper'?",
+    answers: [
+      "Leadership skills",
+      "Stylish dress",
+      "Role as underworld ambassador",
+      "Political acumen"
+    ],
+    correctAnswer: "Stylish dress",
+    tidbit: "Dan Hogan, known as 'Dapper' due to his stylish dress, was a key liaison between local law enforcement and criminal elements in St. Paul."
+  },
+  {
+    question: "Prompt for Roy McCord's search on January 13, 1934?",
+    answers: [
+      "Concerned call from his wife",
+      "Request from his co-worker",
+      "Assignment from the airport",
+      "Anonymous tip"
+    ],
+    correctAnswer: "Concerned call from his wife",
+    tidbit: "McCord's wife called, concerned about strange activity. He enlisted co-worker Robert Luening and met up with Cowin at his 562 Holly Ave. apartment."
+  },
 {
-  question: "Who was the first keeper of John O'Connor's layover agreement system?",
-  answers: ["John Dillinger", "William 'Reddy' Griffin", "'Dapper' Dan Hogan", "Alvin Karpis"],
-  correctAnswer: "William 'Reddy' Griffin",
-  tidbit: "After arriving in town and meeting with the police, criminals stopped to 'check in' with Griffin at the Hotel Savoy in downtown St. Paul. Among his many duties, Griffin collected bribes and brought the money to O'Connor."
-},
-  {
-  question: "What earned Dan Hogan the nickname 'Dapper'?",
-  answers: [
-    "His leadership skills",
-    "His stylish dress",
-    "His role as underworld ambassador",
-    "His political acumen"
-  ],
-  correctAnswer: "His stylish dress",
-  tidbit: "Dan Hogan, affectionately known as 'Dapper' due to his stylish dress, wasn't just a prominent figure in St. Paul's underworld; he was a key liaison between local law enforcement and the criminal elements within the city."
-},
-  {
-  question: "What prompted Roy McCord to search for 'peeping toms' on the night of January 13, 1934?",
-  answers: [
-    "A concerned call from his wife",
-    "A request from his co-worker",
-    "An assignment from the airport",
-    "A tip from an anonymous source"
-  ],
-  correctAnswer: "A concerned call from his wife",
-  tidbit: "Concerned about strange activity, McCord’s wife had called his work and asked him to help look for potential 'peeping toms.' When his shift ended, McCord enlisted co-worker Robert Luening and met up with Cowin at his 562 Holly Ave. apartment."
-},
-  {
-  question: "What prompted the Barker-Karpis Gang to turn to high-profile kidnapping in the aftermath of the 21st Amendment?",
-  answers: [
-    "The success of the Hamm Jr. kidnapping",
-    "To replace income from illegal alcohol trade",
-    "A tip from an anonymous source",
-    "A recommendation from Harry Sawyer"
-  ],
-  correctAnswer: "To replace income from illegal alcohol trade",
-  tidbit: "The December 1933 ratification of the 21st Amendment dried up the illegal alcohol trade, ending an income source for the criminal underworld that needed to be replaced. Hoping to make large amounts of money quickly without the risks associated with robbing banks, the Barker-Karpis Gang turned to high-profile kidnapping."
-},
+  question: "Why did the Barker-Karpis Gang turn to kidnapping post-21st Amendment?",
+  answers: ["Hamm Jr. success", "Replace alcohol income", "Anonymous tip", "Harry Sawyer's recommendation"],
+  correctAnswer: "Replace alcohol income",
+  tidbit: "The 21st Amendment ended illegal alcohol trade, leading the Barker-Karpis Gang to resort to kidnapping for income."
+}
   {
     question: "Which river runs through the Twin Cities of Minneapolis and St. Paul?",
     answers: ["Mississippi River", "Missouri River", "Red River", "Ohio River"],
