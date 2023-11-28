2a26349dd4098b345a98fd50950aa9a1334f46a7
mreicher <42914553+mreicher@users.noreply.github.com>
2023-11-28 17:15:27 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 const questions = [
   {
-    question: "First keeper of John O'Connor's layover agreement system?",
+    question: "Who was the first keeper of John O'Connor's layover agreement system?",
     answers: ["John Dillinger", "William Griffin", "'Dapper' Dan Hogan", "Alvin Karpis"],
     correctAnswer: "William Griffin",
     tidbit: "After arriving in town and meeting with the police, criminals stopped to 'check in' with Griffin at the Hotel Savoy in downtown St. Paul. Griffin collected bribes and brought the money to O'Connor."
@@ -17,27 +17,27 @@ const questions = [
     tidbit: "Dan Hogan, known as 'Dapper' due to his stylish dress, was a key liaison between local law enforcement and criminal elements in St. Paul."
   },
   {
-    question: "Prompt for Roy McCord's search on January 13, 1934?",
+    question: "What was the reason for Roy McCord's search on January 13, 1934?",
     answers: [
-      "Call from his wife",
+      "His wife called",
       "Co-worker request",
       "Work assignment",
       "Anonymous tip"
     ],
-    correctAnswer: "Call from his wife",
+    correctAnswer: "His wife called",
     tidbit: "McCord's wife called, concerned about strange activity. He enlisted co-worker Robert Luening and met up with Cowin at his 562 Holly Ave. apartment."
   },
   {
     question: "Why did the Barker-Karpis Gang turn to kidnapping post-21st Amendment?",
-    answers: ["Hamm Jr. success", "Replace alcohol income", "Anonymous tip", "Harry Sawyer's recommendation"],
+    answers: ["Hamm Jr. success", "Replace alcohol income", "Anonymous tip", "Harry Sawyer's idea"],
     correctAnswer: "Replace alcohol income",
     tidbit: "The 21st Amendment ended illegal alcohol trade, leading the Barker-Karpis Gang to resort to kidnapping for income."
   },
-  {
-    question: "Which river runs through the Twin Cities of Minneapolis and St. Paul?",
-    answers: ["Mississippi River", "Missouri River", "Red River", "Ohio River"],
-    correctAnswer: "Mississippi River",
-    tidbit: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    {
+    question: "Who was Oscar Erickson, and what was his job on December 16, 1932?",
+    answers: ["Cook", "Wreath salesman", "Florist", "Banker"],
+    correctAnswer: "Wreath salesman",
+    tidbit: "Oscar Erickson was a Christmas wreath salesman on December 16, 1932."
   },
   // Add more questions here following the same format
 ];
