14f20b6fc13e91e9115b289c4c4465385b1cb81c
mreicher <42914553+mreicher@users.noreply.github.com>
2023-12-12 20:10:28 -0600

    Create locations_h.js
    Setting up the Hamm's Complex tour.

---
+++ locations_h.js
@@ -0,0 +1,82 @@
+// locations.js
+const locations = [
+      {
+    name: "Minnesota 13 Whiskey",
+    lat: 44.962442294023646,
+    lng: -93.07090362433648,
+    image: "https://www.mnthen.com/tour/hamm/gallery/mn13/hamm_ms13_1.jpg",
+    title: "Minnesota 13",
+    creator: "Matt Reicher",
+    order: "2",
+    htmlFile: "https://mnthen.000webhostapp.com/loc_template.html",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3039852975572-moonshine-legacy-the-story-of-minnesota-13-and-stearns-county-s-bootlegging-era" // Replace with the actual URL of the article
+  },
+   {
+    name: "Swede Hollow",
+    lat: 44.96147364579489,
+    lng: -93.07331340101548,
+    image: "https://www.mnthen.com/tour/hamm/gallery/hollow/hamm_hollow1.jpg",
+    title: "Swede Hollow",
+    creator: "Matt Reicher",
+    htmlFile: "https://mnthen.com/exhibit/locations/stp/swede_hollow.html",
+    order: "6",
+    link: "https://www.mnopedia.org/place/swede-hollow"
+  },
+   {
+    name: "Hamm Jr. Kidnapping (June 15-19, 1933)",
+    lat: 44.9620906324505,
+    lng: -93.0710358080048,
+    image: "https://www.mnthen.com/tour/hamm/gallery/kidnap/hamm_kidnap1.jpg",
+    title: "Hamm Jr. Kidnapping (June 15-19, 1933)",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_kidnap.html",
+    order: "3",
+    link: "https://vocal.media/criminal/kidnapping-of-hamm-s-brewery-president-william-hamm-jr-june-15-19-1933"
+  },
+      {
+    name: "Closing of Stroh Brewery (1997)",
+    lat: 44.96294524306296,
+    lng: -93.07009994914725,
+    image: "https://www.mnthen.com/tour/hamm/gallery/closing/hamm_closing1.jpg",
+    title: "Closing of the Stroh Brewery",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_closing.html",
+    order: "1",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2922345876111-closing-of-east-st-paul-s-stroh-hamm-s-brewery-november-21-1997"
+  },
+    {
+    name: "The Iconic Hamm's Bear (1953 - 99)",
+    lat: 44.96231983329402,
+     lng: -93.07176137488261,
+      image: "https://www.mnthen.com/tour/hamm/gallery/bear/hamm_bear1.jpg",
+    title: "Hamm's Bear",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_bear.html",
+    order: "4",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2918808919766-the-story-of-the-iconic-hamm-s-bear-1953-1999"
+  },          
+  {
+    name: "'New' Hamm's Brewery Public Tour (1894)",
+    lat: 44.96292536959989,
+    lng:  -93.07205849805618,
+    image: "https://www.mnthen.com/tour/hamm/gallery/public/hamm_public1.jpg",
+    title: "Hamm's Brewery Tour",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_tour.html",
+    order: "5",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2928741186662-public-tour-of-the-new-hamm-s-brewery-september-27-1894"
+  },
+    {
+    name: "Theodore Hamm Mansion (1886 - 1954)",
+    lat: 44.961337319371076,
+    lng:  -93.07148153924896,
+     image: "https://www.mnthen.com/tour/hamm/gallery/mansion/hamm_mansion1.jpg",
+     title: "Theodore Hamm Mansion",
+     creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_mansion.html",
+    order: "7",
+    link: "https://mnthen.com/blog/place/stp_hamm_mansion.html"
+  },
+
+  // Add more locations here
+];
