f1db6ac1c9056ce583ec915ce261d3ec779dadab
mreicher <42914553+mreicher@users.noreply.github.com>
2023-12-13 14:59:57 -0600

    Create locations_1.js
    Updating the location list to support the beer and brewing museum niche.

---
+++ locations_1.js
@@ -0,0 +1,125 @@
+// locations.js
+const locations = [
+   {
+    name: "Swede Hollow",
+    lat: 44.96147364579489,
+    lng: -93.07331340101548,
+    image: "https://www.mnthen.com/tour/hamm/gallery/hollow/hamm_hollow1.jpg",
+    title: "Swede Hollow",
+    creator: "Matt Reicher",
+    htmlFile: "https://mnthen.com/exhibit/locations/stp/swede_hollow.html",
+    tour: "hamm",
+    link: "https://www.mnopedia.org/place/swede-hollow"
+  },
+   {
+    name: "Hamm Jr. Kidnapping (June 15-19, 1933)",
+    lat: 44.9620906324505,
+    lng: -93.0710358080048,
+    image: "https://www.mnthen.com/tour/hamm/gallery/kidnap/hamm_kidnap3.jpg",
+    title: "Hamm Jr. Kidnapping (June 15-19, 1933)",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_kidnap.html",
+    tour: "split",
+    link: "https://vocal.media/criminal/kidnapping-of-hamm-s-brewery-president-william-hamm-jr-june-15-19-1933"
+  },
+      {
+    name: "Closing of Stroh Brewery (1997)",
+    lat: 44.96294524306296,
+    lng: -93.07009994914725,
+    image: "https://www.mnthen.com/tour/hamm/gallery/closing/hamm_closing1.jpg",
+    title: "Closing of the Stroh Brewery",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_closing.html",
+    tour: "hamm",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2922345876111-closing-of-east-st-paul-s-stroh-hamm-s-brewery-november-21-1997"
+  },
+    {
+    name: "The Iconic Hamm's Bear (1953 - 99)",
+    lat: 44.96231983329402,
+     lng: -93.07176137488261,
+      image: "https://www.mnthen.com/tour/hamm/gallery/bear/hamm_bear1.jpg",
+    title: "Hamm's Bear",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_bear.html",
+    tour: "hamm",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2918808919766-the-story-of-the-iconic-hamm-s-bear-1953-1999"
+  },          
+  {
+    name: "'New' Hamm's Brewery Public Tour (1894)",
+    lat: 44.96292536959989,
+    lng:  -93.07205849805618,
+    image: "https://www.mnthen.com/tour/hamm/gallery/public/hamm_public1.jpg",
+    title: "Hamm's Brewery Tour",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_tour.html",
+    tour: "hamm",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/2928741186662-public-tour-of-the-new-hamm-s-brewery-september-27-1894"
+  },
+    {
+    name: "Theodore Hamm Mansion (1886 - 1954)",
+    lat: 44.961337319371076,
+    lng:  -93.07148153924896,
+     image: "https://www.mnthen.com/tour/hamm/gallery/mansion/hamm_mansion1.jpg",
+     title: "Theodore Hamm Mansion",
+     creator: "Matt Reicher",
+    htmlFile: "/tour/hamm/hamm_mansion.html",
+    tour: "hamm",
+    link: "https://mnthen.com/blog/place/stp_hamm_mansion.html"
+  },
+      {
+    name: "Stahlmann Brewery (1855 - 97)",
+    lat: 44.92834574749703,
+    lng: -93.12531650959622,
+    image: "https://www.mnthen.com/tour/ftrd/gallery/ftrd_stahl1.jpg",
+    title: "Stahlmann Brewery",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/ftrd/ftrd_stahl.html",
+    tour: "history",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3046980377052-the-legacy-lives-on-uncovering-the-little-known-story-of-christopher-stahlmann-s-brewing-empire"
+  },
+  {
+    name: "Scandal at John's Bar (1952 - 56)",
+    lat: 45.013359183182786,
+     lng:  -93.27190590645603,
+      image: "https://img.particlenews.com/image.php?type=thumbnail_580x000&url=2hntzQ_0mivQGBS00",
+     title: "Scandal at John's Bar (1952 - 56)",
+     creator: "Matt Reicher",
+         htmlFile: "/tour/NE/NE_johnsbar.html",
+        tour: "northeast",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3045699798148-unveiling-the-secrets-of-john-s-bar-and-funhouse-a-scandalous-tale-of-corruption-in-1950s-minneapolis"
+  },
+  {
+    name: "Plantation Night Club (1930 - 1933)",
+    lat: 45.073692373690626,
+    lng:  -93.01483408611226,
+    image: "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant1.jpg",
+    title: "Plantation Night Club",
+    creator: "Matt Reicher",
+    htmlFile: "/tour/wbl/wbl_plant.html",
+    tour: "wbl_split",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club"
+  },
+  {
+    name: "Grain Belt Beer Sign",
+    lat: 44.985500012634446,
+    lng: -93.26562138020033,
+    image: "https://www.mnthen.com/locations/gallery/mpls/premium/mpls_grain_belt_3.jpg",
+    title: "Grain Belt Beer Sign",
+    creator: "Matt Reicher",
+    htmlFile: "/markers/mpls/mpls_grain_belt.html",
+    tour: "northeast",
+    link: "https://mnthen.com/blog/place/mpls_grain_belt.html"
+  },
+  {
+    name: "Gramma B’s (1979 - 1984)",
+    lat: 45.00748720292698,
+    lng: -93.27185181388745,
+    image: "https://www.mnthen.com/locations/gallery/mpls/gramma_b/mpls_gramma_b_1.jpg",
+    title: "Gramma B’s (1979 - 1984)",
+    creator: "Matt Reicher",
+    htmlFile: "/markers/mpls/mpls_gramma_b.html",
+    tour: "northeast",
+    link: "https://www.mnthen.com/blog/place/mpls_gramma_b.html"
+  },
+  // Add more locations here
+];
