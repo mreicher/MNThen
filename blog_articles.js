15c3b2941288c56acdf6c27d8c8e7e4c0ee0fb50
mreicher <42914553+mreicher@users.noreply.github.com>
2024-06-23 20:32:02 -0500

    Create blog_articles.js

---
+++ blog_articles.js
@@ -0,0 +1,162 @@
+const blogArticles = [
+    {
+      headline: "Kidnapping of Edward Bremer (Jan 17 - Feb 7, 1934)",
+      blurb: "Ed Bremer was kidnapped and held for ransom by the Barker-Karpis gang in 1934. His eventual release helped bring about the end of the gangster era.",
+      icon: "fa-calendar",
+      image: "https://mnthen.com/images/gangster_bremer_kidnap1.jpg",
+      url: "https://mnthen.com/blog/event/bremer_kidnap.html",
+      keywords: ["saint paul", "st. paul", "bremer", "kidnap", "kidnapping", "gangster", "all"],
+
+    },
+    {
+      headline: "Bucket of Blood Saloon",
+      blurb: "The Bucket of Blood Saloon in St. Paul’s red-light district gained a reputation fro violence, mirroring the neighborhood’s rough atmosphere.",
+       icon: "fa-building",
+      image: "https://mnthen.com/images/bucketo.jpg",
+      url: "https://mnthen.com/blog/place/bucket_of_blood.html",
+      keywords: ["saint paul", "st. paul", "saloon", "bucket", "all"],
+
+    },
+        {
+      headline: "Shootout at the Lincoln Court Apartments (March 31, 1934)",
+      blurb: "John Dillinger and Homer Van Meter engaged in a gun battle with police at St. Paul's Lincoln Court Apartments, galvanizing the public against gangster violence plaguing the city.",
+        icon: "fa-calendar",
+      image: "https://mnthen.com/images/gangster/gangster_lincoln_ct.jpg",
+      url: "https://mnthen.com/blog/event/shootout_lincoln_ct.html",
+      keywords: ["saint paul", "st. paul", "gangster", "lincoln", "Lincoln", "Dillinger", "dillinger", "all"],
+
+    },
+    {
+      headline: "Shooting of Wreath Salesman Oscar Erickson (Dec. 16, 1932)",
+      blurb: "Oscar Erickson was killed by the Barker-Karpis gang during a robbery getaway, fueling calls to end the era of brazen criminals terrorizing citizens.",
+            icon: "fa-calendar",
+      image: "https://mnthen.com/images/erickson.jpg",
+      url: "https://mnthen.com/blog/people/erickson.html",
+      keywords: ["saint paul", "st. paul", "gangster", "barker", "karpis", "shooting", "all"],
+
+    },
+        {
+      headline: "Original Red's Savoy (1965 - 2017)",
+     blurb: "Iconic local pizza joint Red's Savoy closed in 2017 after 52 years of serving its signature Minnesota-style pizza.",
+           icon: "fa-building",
+      image: "https://mnthen.com/images/reds_savoy.jpg",
+      url: "https://mnthen.com/blog/place/reds_savoy.html",
+      keywords: ["saint paul", "st. paul", "pizza", "Savoy", "savoy", "bar", "all"],
+
+    },
+    {
+      headline: "Humphrey, Hubert H. (1911 - 1978)",
+      blurb: "Progressive champion Hubert Humphrey left an enduring legacy fighting tirelessly for civil rights and social justice as mayor, senator, vice president.",
+            icon: "fa-user",
+      image: "https://mnthen.com/images/humphrey_header.jpg",
+      url: "https://mnthen.com/blog/people/humphrey.html",
+      keywords: ["saint paul", "st. paul", "humphrey", "Humphrey", "politics", "minneapolis", "Minneapolis", "all"],
+
+    },
+        {
+      headline: "Olson, Floyd B. (1891 - 1936)",
+      blurb: "Floyd B. Olson, Minnesota's 22nd Governor, left a profound legacy with his unwavering commitment to social justice, labor rights, and economic reforms during the Great Depression. ",
+            icon: "fa-user",
+      image: "https://mnthen.com/tour/capitol/gallery/olson/olson_1.jpg",
+      url: "https://mnthen.com/blog/people/olson.html",
+      keywords: ["saint paul", "st. paul", "politics", "govenor", "olson", "Olson", "all"],
+
+    },
+        {
+      headline: "Van Meter, Homer V. (1905 - 1934)",
+      blurb: "Van Meter, an infamous Depression-era gangster, met his violent end in a disputed encounter with St. Paul police on August 23, 1934, near University Ave. and Marion St.",
+            icon: "fa-user",
+      image: "https://mnthen.com/images/van_meter_header.jpg",
+      url: "https://mnthen.com/blog/people/van_meter.html",
+      keywords: ["saint paul", "st. paul", "gangster", "dillinger", "Dillinger", "shooting", "van meter", "Van Meter", "all"],
+
+    },
+    {
+      headline: "Grain Belt Beer Sign",
+      blurb: "In 1941, the iconic Grain Belt Beer sign, a giant bottle cap, was erected in Minneapolis. It went dark in 1996 but was relit in 2017 after restoration efforts.",
+            icon: "fa-building",
+      image: "https://mnthen.com/locations/gallery/mpls/premium/mpls_grain_belt_1.jpg",
+      url: "https://mnthen.com/blog/place/mpls_grain_belt.html",
+      keywords: ["premium", "grain belt", "sign", "minneapolis", "Minneapolis", "beer", "all"],
+
+    },
+            {
+      headline: "Gramma B's",
+      blurb: "Gramma B's, a raucous Northeast Minneapolis bar from 1979-1984, attracted bikers and employed bouncers who became famous wrestlers before neighbor complaints eventually helped force its closure.",
+           icon: "fa-building",
+      image: "https://mnthen.com/locations/gallery/mpls/gramma_b/mpls_gramma_b_1.jpg",
+      url: "https://mnthen.com/blog/place/mpls_gramma_b.html",
+      keywords: ["northeast", "Northeast", "Minneapolis", "saloon", "bar", "all"],
+
+    },
+        {
+      headline: "Summit Brewing (1986 - )",
+      blurb: "Founded by Mark Stutrud, Summit grew from a small brewery focusing on underrepresented beer styles to one of Minnesota's largest breweries.",
+            icon: "fa-building",
+      image: "https://mnthen.com/markers/stp/summit/images/stp_summit_1.jpg",
+      url: "https://mnthen.com/blog/place/stp_summit_brewing.html",
+      keywords: ["saint paul", "st. paul", "Summit", "summit", "beer", "stutrud", "Stutrud", "all"],
+
+    },
+    {
+      headline: "Native American Burial Mounds (White Bear Lake)",
+      blurb: "Centuries of Native American history in White Bear Lake area erased in 1889 as sacred burial mounds were demolished to accommodate expanding European settlement.",
+                 icon: "fa-building",
+      image: "https://mnthen.com/tour/wbl/gallery/mound/wbl_mound1.jpg",
+      url: "https://mnthen.com/blog/place/wbl_mounds.html",
+      keywords: ["burial", "mounds", "bear", "Bear", "lake", "all"],
+
+    },
+        {
+      headline: "Wildwood Amusement Park",
+      blurb: "Wildwood Amusement Park on White Bear Lake, the 'Coney Island of Saint Paul,' entertained generations until financial woes forced its closure in the 1930s",
+            icon: "fa-building",
+      image: "https://mnthen.com/locations/gallery/mahtomedi/wildwood/wildwood_2.jpg",
+      url: "https://mnthen.com/blog/place/wildwood.html",
+      keywords: ["amusement", "park", "mahtomedi", "wildwood", "streetcar", "all"],
+
+    },
+   {
+  headline: "Shooting of Roy McCord (Jan. 13, 1934)",
+  blurb: "After work, Roy McCord joined friends to search for 'peeping toms,' inadvertently encountering the Barker-Karpis Gang, leading to his near-fatal shooting.",
+  icon: "fa-calendar",
+  image: "https://mnthen.com/images/gangster/mccord/stp_mccord_holly.jpg",
+  url: "https://mnthen.com/blog/event/stp_mccord_shooting.html",
+  keywords: ["saint paul", "st. paul", "mccord", "gangster", "barker", "karpis", "shooting", "all"],
+},
+       {
+  headline: "Theodore Hamm Mansion (c. 1886 - 1954)",
+  blurb: "The Theodore Hamm mansion once stood majestically over Swede Hollow, embodying St. Paul's brewing heritage and the Hamm family legacy.",
+  icon: "fa-building",
+  image: "https://mnthen.com/tour/hamm/gallery/mansion/hamm_mansion5.jpg",
+  url: "https://mnthen.com/blog/place/stp_hamm_mansion.html",
+  keywords: ["saint paul", "st. paul", "hamm", "mansion", "east side", "all"],
+},
+             {
+      headline: "Clifford, Nina (1851 - 1929)",
+      blurb: "Nina Clifford, a prominent St. Paul madam, operated a high-class brothel and became a wealthy, influential figure, defying societal norms.",
+  icon: "fa-user",
+      image: "https://mnthen.com/images/gangster/clifford/clifford1.jpg",
+      url: "https://mnthen.com/blog/people/clifford.html",
+      keywords: ["brothel", "St. Paul", "Saint Paul", "gangster", "Nina"],
+    },
+                 {
+      headline: "William Hamm Jr. Kidnapping (June 15 - 19, 1933)",
+      blurb: "William Hamm Jr., President of Hamm's Brewing Co., was kidnapped for ransom by the Barker-Karpis Gang in 1933, sparking public outrage and leading to the gang's eventual capture.",
+  icon: "fa-calendar",
+      image: "https://mnthen.com/tour/hamm/gallery/kidnap/hamm_kidnap1.jpg",
+      url: "https://mnthen.com/blog/event/hamm_kidnap.html",
+       keywords: ["kidnapping", "st. paul", "saint paul", "gangster", "Barker", "Karpis"],
+    },
+         {
+      headline: "Death of \"Dapper\" Dan Hogan (Dec 4, 1928)",
+      blurb: "Gangster \"Dapper\" Dan Hogan, a pivotal link between St. Paul's criminal underworld and law enforcement, was killed in a bombing. His murderers remain unidentified.",
+      icon: "fa-calendar",
+      image: "https://mnthen.com/images/gangster/hogan/stp_hogan.jpg",
+      url: "https://mnthen.com/blog/event/stp_hogan.html",
+      keywords: ["saint paul", "st. paul", "hogan", "bomb", "layover", "gangster", "all"],
+    },
+
+
+    // Add more articles as needed
+];
