0ce83d59eba010f330c7c9862e2398d60fb52270
mreicher <42914553+mreicher@users.noreply.github.com>
2024-07-06 13:37:40 -0500

    Update locations_test.js

--- locations_test.js
+++ locations_test.js
@@ -2,17 +2,17 @@
 const locations = [
     {
         id: "1012",
-            lat: 45.073692373690626,
-    lng:  -93.01483408611226,
+        lat: 45.073692373690626,
+        lng:  -93.01483408611226,
         name: "Plantation Night Club (1930 - 1933)",
-            title: "Plantation Night Club",
+        title: "Plantation Night Club",
         image: "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant1.jpg",
         creator: "Matt Reicher",
         city: "White Bear Lake",
-            htmlFile: "/tour/wbl/wbl_plant.html",
-        audioURL: "path/to/your/audio/file.mp3", 
-            tour: "wbl_split",
-    link: "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club"
+        htmlFile: "/tour/wbl/wbl_plant.html",
+        audioURL: "/audio/wbl/wbl_plant.mp3", 
+        tour: "wbl_split",
+        link: "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club"
     },
 
 
