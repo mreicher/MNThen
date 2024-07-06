a045f420ef2c8c196e22088c0b4ef7b0d89f9059
mreicher <42914553+mreicher@users.noreply.github.com>
2024-07-06 11:42:52 -0500

    Update locations_test.js

--- locations_test.js
+++ locations_test.js
@@ -4,7 +4,8 @@ const locations = [
         id: "1012",
         name: "Plantation Night Club (1930 - 1933)",
         image: "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant1.jpg",
-        creator: "Matt Reicher"
+        creator: "Matt Reicher",
+        city: "White Bear Lake",
     },
 
    // Add more locations here
