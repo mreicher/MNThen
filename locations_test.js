59184a834e947dd9494cc3f97ac561944d25520f
mreicher <42914553+mreicher@users.noreply.github.com>
2024-08-17 14:56:51 -0500

    Update locations_test.js

--- locations_test.js
+++ locations_test.js
@@ -15,6 +15,22 @@ const locations = [
         link: "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club",
         imageSource: "Image of the Plantation Nighclub via the White Bear Press"
     },
+        {
+        id: "1013",
+        lat:  45.05648381562563,
+        lng:  -92.96040208835049,
+        name: "Wildwood Amusement Park (1889 - 1932)",
+        title: "Wildwood Amusement Park",
+        image: "https://www.mnthen.com/locations/gallery/mahtomedi/wildwood/wildwood_2.jpg",
+        creator: "Matt Reicher",
+        city: "Mahtomedi",
+        htmlFile: "/markers/mahtomedi/mah_wildwood.html",
+        audioURL: "/audio/mahtomedi/wildwood.mp3", 
+        tours: "history",
+        link: "https://mnthen.com/blog/place/wildwood.html",
+        imageSource: "Image of the slide at Wildwood Park c. 1905 via MPRNews",
+    },
+
 
 
    // Add more locations here
