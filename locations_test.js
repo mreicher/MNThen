e1d17e8980b806e07828f12433d01ec4f904c3f6
mreicher <42914553+mreicher@users.noreply.github.com>
2024-08-22 20:18:28 -0500

    Update locations_test.js
    testing the new location page.

--- locations_test.js
+++ locations_test.js
@@ -1,19 +1,19 @@
 // locations_test.js
 const locations = [
     {
-        id: "1010",
-        lat: 45.073692373690626,
-        lng:  -93.01483408611226,
-        name: "Plantation Night Club (1930 - 1933)",
-        title: "Plantation Night Club",
-        image: "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant1.jpg",
-        creator: "Matt Reicher",
-        city: "White Bear Lake",
-        htmlFile: "/tour/wbl/wbl_plant.html",
-        audioURL: "/audio/wbl/wbl_plant.mp3", 
-        tours: "wbl_split",
-        link: "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club",
-        imageSource: "The Plantation Nighclub via the White Bear Press"
+    id: "1010",
+    lat: 45.14440004252878,
+    lng: -93.00431103417095,
+    name: "Plantation Night Club (1930 - 1933)",
+    title: "Plantation Night Club",
+    image: "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant1.jpg",
+    creator: "Matt Reicher",
+    city: "White Bear Lake",
+    htmlFile: "/tour/wbl/wbl_plant.html",
+    audioURL: "/audio/wbl/wbl_plant.mp3", 
+    tours: "wbl_split",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club",
+    imageSource: "The Plantation Nightclub via the White Bear Press"
     },
         {
         id: "1011",
