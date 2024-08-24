c4c37ced0d8233ff8ab3e9679f302f14bbc5bb96
mreicher <42914553+mreicher@users.noreply.github.com>
2024-08-23 22:52:50 -0500

    Update locations_test.js

--- locations_test.js
+++ locations_test.js
@@ -1,20 +1,6 @@
 // locations_test.js
 const locations = [
-    {
-    "id": "1001",
-    "lat": 45.14437704276625,
-    "lng": -93.00424882606812,
-    "name": "Map Test Location",
-    "title": "Map Test Location",
-    "image": "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant1.jpg",
-    "creator": "Matt Reicher",
-    "city": "White Bear Lake",
-    "htmlFile": "/tour/wbl/wbl_plant.html",
-    "audioURL": "/audio/wbl/wbl_plant.mp3",
-    "tours": "wbl_split",
-    "link": "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club",
-    "imageSource": "The Plantation Nightclub via the White Bear Press"
-},
+
     {
     id: "1010",
     lat: 45.073692373690626,
