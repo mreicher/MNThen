d5d2d90491d5afc4c80e5230620613879bb5e516
mreicher <42914553+mreicher@users.noreply.github.com>
2024-09-25 18:21:55 -0500

    Update locations_h.js

--- example/locations_h.js
+++ example/locations_h.js
@@ -2,8 +2,8 @@ const locations_h = [
     {
         id: 1,
         name: "Location 1",
-        lat: 44.9778,
-        lng: -93.2650,
+        lat: 45.14434783374931,
+        lng: -93.00441148466629,
         image: "https://example.com/image1.jpg",
         audio: "https://example.com/audio1.mp3",
         city: "Minneapolis",
@@ -17,8 +17,8 @@ const locations_h = [
     {
         id: 2,
         name: "Location 2",
-        lat: 44.9537,
-        lng: -93.0900,
+        lat:  45.144664056565475,
+        lng: -93.0030220919916,
         image: "https://example.com/image2.jpg",
         audio: "https://example.com/audio2.mp3",
         city: "St. Paul",
