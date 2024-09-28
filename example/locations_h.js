17e6fa8c72df282adb16e939a08af135c1bd9093
mreicher <42914553+mreicher@users.noreply.github.com>
2024-09-27 21:46:52 -0500

    Update locations_h.js

--- example/locations_h.js
+++ example/locations_h.js
@@ -1,22 +1,155 @@
 const locations_h = [
+
     {
-        id: 1,
-        name: "Location 1",
-        lat: 45.14434783374931,  
-        lng: -93.00441148466629, 
-        image: "https://mnthen.com/images/nyes.jpg",
-        audio: "https://example.com/audio1.mp3",
-        city: "Minneapolis",
-        creator: "Matt Reicher"
+    id: "1010",
+    lat: 45.073692373690626,
+    lng: -93.01483408611226,
+    name: "Plantation Night Club (1930 - 1933)",
+    title: "Plantation Night Club",
+    image: "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant1.jpg",
+    creator: "Matt Reicher",
+    city: "White Bear Lake",
+    audioURL: "/audio/wbl/wbl_plant.mp3", 
+    tours: "wbl_split",
+    link: "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club",
+    imageSource: "The Plantation Nightclub via the White Bear Press"
     },
+    
+        {
+        id: "1011",
+        lat:  45.05648381562563,
+        lng:  -92.96040208835049,
+        name: "Wildwood Amusement Park (1889 - 1932)",
+        title: "Wildwood Amusement Park",
+        image: "https://www.mnthen.com/locations/gallery/mahtomedi/wildwood/wildwood_2.jpg",
+        creator: "Matt Reicher",
+        city: "Mahtomedi",
+        audioURL: "/audio/mahtomedi/wildwood.mp3", 
+        tours: "history",
+        link: "https://mnthen.com/blog/place/wildwood.html",
+        imageSource: "Image of the slide at Wildwood Park c. 1905 via MPRNews"
+    },
+    
+{
+    id: "1012",
+    lat: 44.922167523742054,
+    lng: -93.10991227113067,
+    name: "Michael 'Eyedea' Larsen (1981 - 2010)",
+    title: "Michael 'Eyedea' Larsen",
+    image: "https://www.mnthen.com/images/wsp_eyedea.jpg",
+    creator: "Matt Reicher",
+    city: "St. Paul",
+    audioURL: "/audio/wsp/wsp_eyedea.mp3", 
+    tours: "history",
+    link: "https://mnthen.com/blog/people/eyedea_larsen.html",
+    imageSource: "Image of 'Eyedea' Larsen via Heavy Blog is Heavy"
+},
+    
     {
-        id: 2,
-        name: "Location 2",
-        lat: 45.144664056565475, 
-        lng: -93.0030220919916,  
-        image: "https://example.com/image2.jpg",
-        audio: "https://example.com/audio2.mp3",
-        city: "St. Paul",
-        creator: "Matt Reicher"
-    }
+    id: "1013",
+    lat: 44.1680805962643,
+    lng: -94.00311590799822,
+    name: "Hanging of the Dakota 38 (Dec 26, 1862)",
+    title: "Hanging of the Dakota 38",
+    image: "https://mnthen.com/markers/mankato/images/mankato_38.jpg",
+    creator: "Matt Reicher",
+    city: "Mankato",
+    audioURL: "/audio/mankato/man_dakota_38.mp3", 
+    tours: "history",
+    link: "https://mnthen.com/blog/event/dakota_conflict.html",
+    imageSource: "The Dakota 38 Memorial via Library of Congress"
+},
+    
+{
+    id: "1014",
+    lat: 44.961337319371076,
+    lng: -93.07148153924896,
+    name: "Theodore Hamm Mansion (c. 1886 - 1954)",
+    title: "Theodore Hamm Mansion",
+    image: "https://mnthen.com/tour/hamm/gallery/mansion/hamm_mansion5.jpg",
+    creator: "Matt Reicher",
+    city: "St. Paul",
+    audioURL: "/exhibit/audio/stp/stp_hamm_mansion.mp3", 
+    tours: "hamm",
+    link: "https://mnthen.com/blog/place/stp_hamm_mansion.html",
+    imageSource: "The Hamm Mansion via 1889 Victorian House Restoration"
+},
+    
+    {
+    id: "1015",
+    lat: 44.98463074655949,
+    lng: -93.26445659478223,
+    name: "Father Louis Hennepin Suspension Bridge",
+    title: "Father Louis Hennepin Suspension Bridge",
+    image: "/locations/gallery/mpls/henn_bridge/henn_bridge_1.jpg",
+    creator: "Matt Reicher",
+    city: "Minneapolis",
+    audioURL: "/audio/mpls/mpls_henn_bridge.mp3", 
+    tours: "history",
+    link: "https://www.mnopedia.org/structure/father-louis-hennepin-suspension-bridge",
+    imageSource: "First suspension bridge in Minneapolis, (c. 1858) via MNopedia"
+},
+    
+{
+    "id": "1016",
+    "lat": 44.95463990008291,
+    "lng": -93.10223181529494,
+    "name": "Minnesota’s Third State Capitol",
+    "title": "Minnesota’s Third State Capitol",
+    "image": "https://mnthen.com/exhibit/images/capitol3.jpg",
+    "creator": "Brian C. Pease",
+    "city": "St. Paul",
+    "audioURL": "/audio/exhibit/capitol3.mp3",
+    "tours": "capitol",
+    "link": "https://www.mnopedia.org/structure/minnesota-s-third-state-capitol",
+    "imageSource": "Minnesota State Capitol Building (2022)"
+},
+    
+{
+  "id": "1017",
+  "lat": 44.893943004662326,
+  "lng": -93.18772007741772,
+  "name": "Whiskey the Horse (c. 1911 - 1943)",
+  "title": "Whiskey the Horse (c. 1911 - 1943)",
+  "image": "https://mnthen.com/images/whiskey.jpg",
+  "creator": "Matt Reicher",
+  "city": "St. Paul",
+  "audioURL": "/audio/exhibit/whiskey.mp3",
+  "tours": "history",
+  "link": "https://mnthen.com/blog/people/whiskey.html",
+  "imageSource": "Whiskey with Captain W.R. Hazelrigg (1943)"
+},
+
+    {
+    "id": "1018",
+    "lat": 44.96153652405189,
+    "lng": -93.07320192008716,
+    "name": "Swede Hollow (Pre-1839: Unknown - 1956)",
+    "title": "Swede Hollow",
+    "image": "https://mnthen.com/tour/hamm/gallery/hollow/hamm_hollow1.jpg",
+    "creator": "Matt Reicher",
+    "city": "St. Paul",
+    "audioURL": "/audio/exhibit/swede_hollow.mp3",
+    "tours": "history",
+    "link": "https://www.mnopedia.org/place/swede-hollow",
+    "imageSource": "Swede Hollow looking north, (c.1910). via MNopedia"
+},
+
+{
+    "id": "1019",
+    "lat": 44.956571587648774,
+    "lng": -93.10028134083977,
+    "name": "William R. Merriam Mansion (1882 - 1896)",
+    "title": "William R. Merriam Mansion",
+    "image": "https://mnthen.com/images/merriam_house.jpg",
+    "creator": "Matt Reicher",
+    "city": "St. Paul",
+    "audioURL": "/audio/exhibit/merriam_mansion.mp3",
+    "tours": "history",
+    "link": "https://mnthen.com/blog/place/merriam_mansion.html",
+    "imageSource": "William R. Merriam residence, (c.1890). via St Paul Historical"
+},
+
+
+   // Add more locations here
 ];
