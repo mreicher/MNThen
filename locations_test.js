26b7570216aee4da19204d10cf4656579e9b180d
mreicher <42914553+mreicher@users.noreply.github.com>
2024-10-27 12:12:24 -0500

    Update locations_test.js

--- locations_test.js
+++ locations_test.js
@@ -1,25 +1,6 @@
 const locations = [
 
     {
-        id: "1010",
-        lat: 45.073692373690626,
-        lng: -93.01483408611226,
-        name: "Plantation Night Club (1930 - 1933)",
-        title: "Plantation Night Club",
-        image: "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant1.jpg",
-        image2: "https://www.mnthen.com/tour/wbl/gallery/plant/wbl_plant3.jpg",
-        creator: "Matt Reicher",
-        city: "White Bear Lake",
-        audio: "/audio/wbl/wbl_plant.mp3", 
-        tours: "wbl_split",
-        summary: "This is a placeholder.",
-        link: "https://original.newsbreak.com/@ron-dansley-1587459/3078633820088-lion-s-park-a-remembrance-of-white-bear-lake-s-legendary-plantation-night-club",
-        imageSource: "Plantation Nightclub via the White Bear Press",
-        imageSource2: "Plantation Nightclub 1952 aerial view via the White Bear Press",
-        additionalInfo: "This is a place to add more info. This is a placeholder for now."
-    },
-
-    {
         id: "1011",
         lat: 45.05648381562563,
         lng: -92.96040208835049,
@@ -47,10 +28,10 @@ const locations = [
         city: "St. Paul",
         audio: "/audio/wsp/wsp_eyedea.mp3", 
         tours: "history",
-                summary: "This is a placeholder.",
+        summary: "Michael "Eyedea" Larsen was a talented battle rapper and hip-hop artist from St. Paul. He passed away at 28, but his memory lives on through his music.",
         link: "https://mnthen.com/blog/people/eyedea_larsen.html",
         imageSource: "Image of 'Eyedea' Larsen via Heavy Blog is Heavy",
-        additionalInfo: "This is a place to add more info. This is a placeholder for now."
+        additionalInfo: "Eyedea's music often delved into philosophy, psychology, and social issues, reflecting his thoughtful and introspective nature."
     },
 
     {
@@ -64,10 +45,10 @@ const locations = [
         city: "Mankato",
         audio: "/audio/mankato/man_dakota_38.mp3", 
         tours: "history",
-                summary: "This is a placeholder.",
+        summary: "The 1862 Dakota War culminated in the largest mass hanging in US history, with 38 Dakota warriors executed, marking the end of Native American resistance in the region.",
         link: "https://mnthen.com/blog/event/dakota_conflict.html",
         imageSource: "The Dakota 38 Memorial via Library of Congress",
-        additionalInfo: "This is a place to add more info. This is a placeholder for now."
+        additionalInfo: "After the U.S.-Dakota War, 2,000 Dakota were captured; President Lincoln commuted the sentences of all but 39 of the 303 condemned to die."
     },
 
     {
@@ -81,10 +62,10 @@ const locations = [
         city: "St. Paul",
         audio: "/exhibit/audio/stp/stp_hamm_mansion.mp3", 
         tours: "hamm",
-                summary: "This is a placeholder.",
+        summary: "The Theodore Hamm mansion, a grand St. Paul landmark, was a symbol of the Hamm family's wealth. Built in the late 1800s, it was destroyed by fire in 1954.",
         link: "https://mnthen.com/blog/place/stp_hamm_mansion.html",
         imageSource: "The Hamm Mansion via 1889 Victorian House Restoration",
-        additionalInfo: "This is a place to add more info. This is a placeholder for now."
+        additionalInfo: "In 1976, the site of the Theodore Hamm Mansion became Upper Swede Hollow Park, honoring the family's contributions to St. Paul."
     },
 
     {
@@ -98,10 +79,10 @@ const locations = [
         city: "Minneapolis",
         audio: "/audio/mpls/mpls_henn_bridge.mp3", 
         tours: "history",
-                summary: "This is a placeholder.",
+        summary: "The Father Louis Hennepin Bridge, built in 1855, was the first to span the Mississippi River above St. Anthony Falls. It connected Minneapolis to the east bank, spurring incredible growth.",
         link: "https://www.mnopedia.org/structure/father-louis-hennepin-suspension-bridge",
         imageSource: "First suspension bridge in Minneapolis, (c. 1858) via MNopedia",
-        additionalInfo: "This is a place to add more info. This is a placeholder for now."
+        additionalInfo: "The original bridge was a suspension bridge, a relatively new and innovative design at the time."
     },
 
     {
@@ -115,10 +96,10 @@ const locations = [
         city: "St. Paul",
         audio: "/audio/exhibit/capitol3.mp3",
         tours: "capitol",
-                summary: "This is a placeholder.",
+        summary: "Minnesota’s third state capitol, built between 1896 and 1905 by architect Cass Gilbert, is a stunning mix of artistry and innovation. Recent restorations preserved its historical beauty while adding modern updates.",
         link: "https://www.mnopedia.org/structure/minnesota-s-third-state-capitol",
         imageSource: "Minnesota State Capitol Building (2022)",
-        additionalInfo: "This is a place to add more info. This is a placeholder for now."
+        additionalInfo: "The Capitol’s dome is the second-largest self-supported marble dome in the world, after St. Peter's Basilica in Vatican City."
     },
 
     {
@@ -132,10 +113,10 @@ const locations = [
         city: "St. Paul",
         audio: "/audio/exhibit/whiskey.mp3",
         tours: "history",
-                summary: "This is a placeholder.",
+        summary: "Whiskey, a rebellious Army horse, became a legendary performer and jumper under Lt. Hazelrigg's training at Fort Snelling, maintaining their bond until his death at 32 in 1943.",
         link: "https://mnthen.com/blog/people/whiskey.html",
         imageSource: "Whiskey with Captain W.R. Hazelrigg (1943)",
-        additionalInfo: "This is a place to add more info. This is a placeholder for now."
+        additionalInfo: "Whiskey was buried with full military honors, marking the end of an era where horses played a significant role in the U.S. Army."
     },
 
     {
@@ -149,10 +130,10 @@ const locations = [
         city: "St. Paul",
         audio: "/audio/exhibit/swede_hollow.mp3",
         tours: "history",
-                summary: "This is a placeholder.",
+        summary: "Swede Hollow was a hidden immigrant neighborhood in St. Paul from the 1850s to 1956. Despite no plumbing or electricity, it became a tight-knit, hardworking community.",
         link: "https://www.mnopedia.org/place/swede-hollow",
         imageSource: "Swede Hollow looking north, (c.1910). via MNopedia",
-        additionalInfo: "This is a place to add more info. This is a placeholder for now."
+        additionalInfo: "In 1976, Swede Hollow was transformed into a nature sanctuary with trails and signs commemorating the communities that once lived there."
     },
 
     {
@@ -166,10 +147,10 @@ const locations = [
         city: "St. Paul",
         audio: "/audio/exhibit/merriam_mansion.mp3",
         tours: "history",
-                summary: "This is a placeholder.",
+        summary: "In 1880, William R. Merriam built an impressive Queen Anne-style mansion in St. Paul, designed by Clarence Johnston. The mansion, a social hub, was tragically destroyed by fire in 1896.",
         link: "https://mnthen.com/blog/place/merriam_mansion.html",
         imageSource: "William R. Merriam residence, (c.1890). via St Paul Historical",
-        additionalInfo: "This is a place to add more info. This is a placeholder for now."
+        additionalInfo: "After serving as Minnesota's governor, Merriam was appointed by President McKinley to direct the U.S. Census Bureau in 1899."
     }
 
     // Add more locations here
