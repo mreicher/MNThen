601da7513833ef81797c31af2a2d56f084bd70e2
mreicher <42914553+mreicher@users.noreply.github.com>
2024-11-15 14:40:42 -0600

    Create firebase-config.js

---
+++ example/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyB39tWEJsbd73MzhFUjoxEXfiTEoCM8ZzM",
+  authDomain: "capitol-inventory.firebaseapp.com",
+  projectId: "capitol-inventory",
+  storageBucket: "capitol-inventory.appspot.com",
+  messagingSenderId: "663001635603",
+  appId: "1:663001635603:web:2eda30ee0db3114b0198e6",
+  storageCorsOrigins: ["https://mnthen.com"]
+};
+
+export default firebaseConfig;
