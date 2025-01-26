fa2423072f23734cd4fbec152155fac03138835b
mreicher <42914553+mreicher@users.noreply.github.com>
2025-01-26 11:40:06 -0600

    Create locations_h.js

---
+++ tour/gangster/locations_h.js
@@ -0,0 +1,70 @@
+const locations_h = [
+        {
+        id: 1,
+        name: "Barker-Karpis Gang West Saint Paul Hideout",
+        lat: 44.915278600994434,
+        lng: -93.08081040735343,
+        image: "https://mnthen.com/tour/gangster/images/wsp_house.jpg",
+        audio: "https://mnthen.com/audio/tour/gangster/robert.mp3",
+        city: "West Saint Paul",
+        creator: "Matt Reicher",
+        link: "http://www.historictwincities.com/2021/07/12/barker-karpis-gang-hideout-house-1031-south-robert-street/",
+        imageSource: "Former Barker-Karpis WSP Hideout via <a href='https://www.atlasobscura.com/places/barker-karpis-hideout-house' target=_blank'>Atlas Obscura</a>.",
+        trivia: {
+            question: "What was the name of Nick Hannegraf's Tavern?",
+            options: ["Drover's", "Nick's", "Grover's", "Cheers"],
+            answer: 0
+        }
+    },
+    {
+        id: 2,
+        name: "O'Connor Layover Agreement",
+        lat: 44.94347828994008,
+        lng:  -93.09743701161382,
+        image: "https://mnthen.com/images/gangster/layover/stp_oconnor_1.jpg",
+        audio: "https://mnthen.com/audio/tour/gangster/oconnor.mp3",
+        city: "Saint Paul",
+        creator: "Matt Reicher",
+        link: "https://www.mnopedia.org/thing/oconnor-layover-agreement",
+        imageSource: "John O’Connor during the height of his power in St. Paul, c. 1912. via MNHS",
+        trivia: {
+            question: "What year did Dan Hogan took over as O'Connor Agreement liaison?",
+            options: ["1902", "1913", "1919", "1924"],
+            answer: 1
+        }
+    },
+    {
+        id: 3,
+        name: "Death of 'Dapper' Dan Hogan",
+        lat: 44.91686647393094,
+        lng:  -93.1429757197338,
+        image: "https://mnthen.com/images/gangster/hogan/stp_hogan.jpg",
+        audio: "https://mnthen.com/audio/tour/gangster/hogan.mp3",
+        city: "Saint Paul",
+        creator: "Matt Reicher",
+        link: "https://mnthen.com/blog/event/stp_hogan.html",
+        imageSource: "Saint Paul Pioneer Press; Dec 5, 1928 via SPPHS",
+        trivia: {
+            question: "Why was Dan Hogan nicknamed 'Dapper'?",
+            options: ["Impressive smile", "His classic car", "Stylish Dress", "Intelligence"],
+            answer: 2
+        }
+    },
+    {
+        id: 4,
+        name: "Shooting of Roy McCord",
+        lat: 44.943518677677204,
+        lng: -93.13142857825974,
+        image: "https://mnthen.com/images/gangster/mccord/stp_mccord_holly.jpg",
+        audio: "https://mnthen.com/audio/tour/gangster/mccord.mp3",
+        city: "Saint Paul",
+        creator: "Matt Reicher",
+        link: "https://mnthen.com/blog/event/stp_mccord_shooting.html",
+        imageSource: "562 Holly Ave. (2013) via the author",
+        trivia: {
+            question: "What was Roy McCord's occupation?",
+            options: ["Pilot", "Custodian", "Radio operator", "Police Officer"],
+            answer: 2
+        }
+    },
+];
