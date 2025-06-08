51733063cf61aa38f842829d42262a8c29410464
mreicher <42914553+mreicher@users.noreply.github.com>
2025-06-07 23:02:00 -0500

    Update blog_articles.js

--- blog_articles.js
+++ blog_articles.js
@@ -3,6 +3,8 @@ const blogArticles = [
   "headline": "Molter, Dorothy 'The Root Beer Lady' (1907 - 1986)",
   "blurb": "Defying isolation, she was the last non-indeginous full-time BWCAW resident, brewing 10,000 bottles of root beer annually for resort visitors.",
   "icon": "fa-user",
+  "author": "Matt Reicher",
+  "date": "August 1. 2020",
   "image": "https://mnthen.com/images/molter.jpg",
   "url": "https://mnthen.com/blog/people/molter.html",
   "keywords": ["root beer", "BWCA", "Northwoods", "Boundary Waters", "Root Beer Lady", "person", "all"]
