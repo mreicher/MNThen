a7028bd4255da6213ebdd3b3b3f571e7a7d982b4
mreicher <42914553+mreicher@users.noreply.github.com>
2025-06-12 17:03:07 -0500

    Update blog_articles.js

--- blog_articles.js
+++ blog_articles.js
@@ -200,7 +200,7 @@ const blogArticles = [
     "keywords": ["minneapolis", "mpls", "bicycle", "bike", "race", "dottie", "person", "all"]
   },
   {
-    "headline": "St. Paul's Downtown Ball Park (aka the 'Pillbox') 1903 - 1909",
+    "headline": "St. Paul's Downtown Ball Park (The 'Pillbox') 1903 - 1909",
     "blurb": "St. Paul's Downtown Ballpark, known as 'The Pillbox,' served as the St. Paul Saints' home from 1903-1909. This compact field, built to keep the team downtown, featured unique dimensions and close-quarters play.",
     "icon": "fa-building",
     "author": "Matt Reicher",
