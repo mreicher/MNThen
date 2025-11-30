7229905c9d89b49f0b52fb797ea116b9bd87a75c
mreicher <42914553+mreicher@users.noreply.github.com>
2025-11-30 16:35:17 -0600

    Update locations_h.js

--- tour/gangster/locations_h.js
+++ tour/gangster/locations_h.js
@@ -1,5 +1,5 @@
-const locations_h = [
-        {
+window.locations_h = [
+    {
         id: 1,
         name: "Barker-Karpis Gang West Saint Paul Hideout",
         lat: 44.915278600994434,
@@ -9,7 +9,7 @@ const locations_h = [
         city: "West Saint Paul",
         creator: "Matt Reicher",
         link: "http://www.historictwincities.com/2021/07/12/barker-karpis-gang-hideout-house-1031-south-robert-street/",
-        imageSource: "Former Barker-Karpis WSP Hideout via <a href='https://www.atlasobscura.com/places/barker-karpis-hideout-house' target=_blank'>Atlas Obscura</a>.",
+        imageSource: "Former Barker-Karpis WSP Hideout via <a href='https://www.atlasobscura.com/places/barker-karpis-hideout-house' target=_blank>Atlas Obscura</a>.",
         trivia: {
             question: "What was the name of Nick Hannegraf's Tavern?",
             options: ["Drover's", "Nick's", "Grover's", "Cheers"],
@@ -20,7 +20,7 @@ const locations_h = [
         id: 2,
         name: "O'Connor Layover Agreement",
         lat: 44.94347828994008,
-        lng:  -93.09743701161382,
+        lng: -93.09743701161382,
         image: "https://mnthen.com/images/gangster/layover/stp_oconnor_1.jpg",
         audio: "https://mnthen.com/audio/tour/gangster/oconnor.mp3",
         city: "Saint Paul",
@@ -37,7 +37,7 @@ const locations_h = [
         id: 3,
         name: "Death of 'Dapper' Dan Hogan",
         lat: 44.91686647393094,
-        lng:  -93.1429757197338,
+        lng: -93.1429757197338,
         image: "https://mnthen.com/images/gangster/hogan/stp_hogan.jpg",
         audio: "https://mnthen.com/audio/tour/gangster/hogan.mp3",
         city: "Saint Paul",
@@ -66,5 +66,5 @@ const locations_h = [
             options: ["Pilot", "Custodian", "Radio operator", "Police Officer"],
             answer: 2
         }
-    },
+    }
 ];
