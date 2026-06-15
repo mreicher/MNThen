1eb17f5ca9253a97fbe9c3257f158ba6f8c619d3
mreicher <42914553+mreicher@users.noreply.github.com>
2026-06-15 11:55:37 -0500

    Update locations_main.js

--- locations_main.js
+++ locations_main.js
@@ -211,8 +211,8 @@ const locations = [
 
    {
         id: "1021",
-        lat: 45.144406427672635, 
-        lng: -93.00438694690537,
+        lat: 44.95655191090327,
+        lng: -93.1035066975175,
         name: "Prom Ballroom (1941 - 1987) test",
         title: "Prom Ballroom",
         image: "https://mnthen.com/images/prom1.webp",
